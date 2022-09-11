180119c7d31f1762528c710bf01ffd52f836eb22
Tanya Babina <111501203+tanyush2511@users.noreply.github.com>
2022-09-12 00:32:26 +0300

    создали цикл, чтобы пройти по рядам, поменяли формулу во вложенном цикле, поменяли размер окна

--- first/snake_1.py
+++ first/snake_1.py
@@ -3,8 +3,8 @@ import pygame
 SIZE_BLOCK = 20
 FRAME_COLOR = (0, 255, 204)
 WHITE = (255, 255, 255)
-BLUE = (204,255,255)
-size = [400, 600]
+BLUE = (204, 255, 255)
+size = [500, 600]
 COUNT_BLOKS = 20
 MARGIN = 1
 screen = pygame.display.set_mode(size)
@@ -17,12 +17,15 @@ while True:
 
     screen.fill(FRAME_COLOR)
 
-    for column in range(COUNT_BLOKS):
-        if column%2 == 0:
-            color = BLUE
-        else:
-            color = WHITE
-        pygame.draw.rect(screen, color, [10 + column*SIZE_BLOCK + MARGIN*(column+1), 20, SIZE_BLOCK, SIZE_BLOCK])
-
+    for row in range(COUNT_BLOKS):
+        for column in range(COUNT_BLOKS):
+            if (row + column) % 2 == 0:
+                color = BLUE
+            else:
+                color = WHITE
+            pygame.draw.rect(screen, color,
+                             [10 + column * SIZE_BLOCK + MARGIN * (column + 1),
+                              20 + row * SIZE_BLOCK + MARGIN * (row + 1),
+                              SIZE_BLOCK, SIZE_BLOCK])
 
     pygame.display.flip()
