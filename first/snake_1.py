179000376c32f3944a00729fee0a7ee630397e0f
Tanya Babina <111501203+tanyush2511@users.noreply.github.com>
2022-09-12 00:19:36 +0300

    создали цикл и обходили по колонкам чет/нечетные, чтобы применить color WHITE or BLUE

--- first/snake_1.py
+++ first/snake_1.py
@@ -1,10 +1,11 @@
 import pygame
 
 SIZE_BLOCK = 20
-FRAME_COLOR = (0,255,204)
-WHITE = (255,255,255)
-
-size = [400,600]
+FRAME_COLOR = (0, 255, 204)
+WHITE = (255, 255, 255)
+BLUE = (204,255,255)
+size = [400, 600]
+COUNT_BLOKS = 20
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Змейка")
 while True:
@@ -14,5 +15,13 @@ while True:
             pygame.quit()
 
     screen.fill(FRAME_COLOR)
-    pygame.draw.rect(screen, WHITE, [10, 20,SIZE_BLOCK,SIZE_BLOCK])
+
+    for column in range(COUNT_BLOKS):
+        if column%2 == 0:
+            color == BLUE
+        else:
+            color == WHITE
+        pygame.draw.rect(screen, color, [10 + column*SIZE_BLOCK, 20, SIZE_BLOCK, SIZE_BLOCK])
+
+
     pygame.display.flip()
