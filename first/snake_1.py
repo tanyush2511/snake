f9b34fe1751431c9f1f3e4977d571579b2c9498a
Tanya Babina <111501203+tanyush2511@users.noreply.github.com>
2022-09-12 00:56:07 +0300

    аддаптировали наши ряды и колонки под размер игрового окна

--- first/snake_1.py
+++ first/snake_1.py
@@ -4,9 +4,14 @@ SIZE_BLOCK = 20
 FRAME_COLOR = (0, 255, 204)
 WHITE = (255, 255, 255)
 BLUE = (204, 255, 255)
-size = [500, 600]
-COUNT_BLOKS = 20
+HEADER_COLOR = (0, 204, 153)
+COUNT_BLOCKS = 20
 MARGIN = 1
+HEADER_MARGIN = 70
+size = [SIZE_BLOCK * COUNT_BLOCKS + 2 * SIZE_BLOCK + MARGIN * COUNT_BLOCKS,
+        SIZE_BLOCK * COUNT_BLOCKS + 2 * SIZE_BLOCK + MARGIN * COUNT_BLOCKS + HEADER_MARGIN]
+print(size)
+
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Змейка")
 while True:
@@ -16,16 +21,17 @@ while True:
             pygame.quit()
 
     screen.fill(FRAME_COLOR)
+    pygame.draw.rect(screen, HEADER_COLOR, [0, 0, size[0], HEADER_MARGIN])
 
-    for row in range(COUNT_BLOKS):
-        for column in range(COUNT_BLOKS):
+    for row in range(COUNT_BLOCKS):
+        for column in range(COUNT_BLOCKS):
             if (row + column) % 2 == 0:
                 color = BLUE
             else:
                 color = WHITE
             pygame.draw.rect(screen, color,
-                             [10 + column * SIZE_BLOCK + MARGIN * (column + 1),
-                              20 + row * SIZE_BLOCK + MARGIN * (row + 1),
+                             [SIZE_BLOCK + column * SIZE_BLOCK + MARGIN * (column + 1),
+                              HEADER_MARGIN + SIZE_BLOCK + row * SIZE_BLOCK + MARGIN * (row + 1),
                               SIZE_BLOCK, SIZE_BLOCK])
 
     pygame.display.flip()
