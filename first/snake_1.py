a0133e73f95ee6eb20a3307e59518b5cf643e9af
Tanya Babina <111501203+tanyush2511@users.noreply.github.com>
2022-09-11 23:51:59 +0300

    поменяли размер окна, добавили выход

--- first/snake_1.py
+++ first/snake_1.py
@@ -1,10 +1,11 @@
 import pygame
 
 
-size = [600,800]
+size = [400,600]
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Змейка")
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            print("exit")
             pygame.quit()
